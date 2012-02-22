e09b38c7e1dfb72f81771efc3a9bd7f2c07c8162
Semih Cemiloglu <semih.cemiloglu@gmail.com>
2012-02-22 23:48:30 +1100

    introduce PRS_HOME directory

--- setenv.sh
+++ setenv.sh
@@ -2,6 +2,11 @@
 # Necessary environment variables to work QmkBld based projects
 #
 
+# If Projects Home directory is not defined
+if [ -z "$PRJS_HOME" ]; then
+  PRJS_HOME=$HOME
+fi
+
 # QmkBld related variables
 QMKBLD=$HOME/qmkbld
 QMK_DEBUG=0
@@ -11,10 +16,11 @@ export QMKBLD QMK_DEBUG QMAKEFEATURES
 # The project that uses QmkBld 
 export PRJ_ROOT=$HOME/qt-bdb
 
-# Berkeley DB root
+# Berkeley DB root directory
 export PRJ_BDB=$PRJ_ROOT/bdb
 
 export LD_LIBRARY_PATH=$PRJ_BDB/lib:$LD_LIBRARY_PATH
 
-
-
+# Give feedback about project directories
+echo "PRJ_ROOT=$PRJ_ROOT"
+echo "PRJ_BDB=$PRJ_BDB"
