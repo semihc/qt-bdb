b2e3188b173c4396bb2f5220323911cb1d026e34
unknown <semihc@ural.(none)>
2012-04-12 19:27:52 +1000

    PATH must be set to access BDB dll files.

--- setenv.bat
+++ setenv.bat
@@ -15,7 +15,11 @@ rem The project that uses QmkBld
 set PRJ_ROOT=%PRJS_HOME%\qt-bdb
 
 rem Berkeley DB root
-set PRJ_BDB=$PRJ_ROOT\bdb
+set PRJ_BDB=%PRJ_ROOT%\bdb
+
+rem Access to BDB libraries is set via
+set DB_WIN32=%PRJ_BDB%\db-5.3.15\build_windows\Win32
+set PATH=%DB_WIN32%\Debug;%DB_WIN32%\Release;%PATH%
 
 rem Give feedback about project root
 @echo PRJ_ROOT=%PRJ_ROOT%
